d655567850842ad59a35ff4bd3f9b92e5496c04f
midia-lima <midialp2@gmail.com>
2024-12-20 20:10:21 -0300

    aulas

---
+++ aula06/Argumentos2.java
@@ -0,0 +1,11 @@
+package aula06; 
+
+public class Argumentos2 {
+
+  public static void main(String[] args) {
+
+    System.out.println("Olá Mundo! " + args[0]);
+
+  }
+
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,9 @@
+package aula07;
+
+public class Erro03 {
+
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo");
+  }
+
+}
